9ff9b1795ab79289678ffa2edf5af48015b18eb7
root <sam.magrath@trademe.co.nz>
2023-03-01 22:38:29 +0000

    Added the first label properly

--- main.tf
+++ main.tf
@@ -50,9 +50,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  labels       = {
-    department = "devops"
-  }
 
   boot_disk {
     initialize_params {
@@ -74,6 +71,7 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    deparment = "devops"
   }
 
 }
