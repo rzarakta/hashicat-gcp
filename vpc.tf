7b07387ca6b39335859faa5f0a5afa1200e6f75e
root <sam.magrath@trademe.co.nz>
2023-03-01 23:11:07 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.0"
 
-    project_id   = var.project_id
+    project_id   = var.project
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
