ef940ea797eddf57b02b8caabb62eb6da66058f2
root <sam.magrath@trademe.co.nz>
2023-03-02 00:01:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,44 +1,14 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-        }
-    ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
+module "network" {
+  source  = "app.terraform.io/sammy-terraform/network/google"
+  version = "6.0.1"
+  network_name = "gaurav-network"
+  project_id = var.project_id
+  
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
