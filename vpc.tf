02f3b4c8985ddffb338b0195d6c0c44b3347423c
root <sam.magrath@trademe.co.nz>
2023-03-02 00:08:57 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 module "network" {
   source  = "app.terraform.io/sammy-terraform/network/google"
   version = "3.4.0"
-  network_name = "gaurav-network"
+  network_name = "gaurav-2-network"
   project_id = var.project
 
   subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "gaurav-2-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
